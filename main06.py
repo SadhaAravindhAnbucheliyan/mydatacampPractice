67c327fda206edaec014e3ac28d65748c4a3de8c
SadhaAravindhAnbucheliyan <aravindhsadha0112@gmail.com>
2022-03-30 13:26:19 +0530

    numpy

---
+++ main06.py
@@ -0,0 +1,8 @@
+height=[98,99,97,96,95,94]
+weight=[65,64,69,68,67,66]
+import numpy as np
+np_height_m=np.array(height)
+np_weight_kg=np.array(weight)
+
+print(np_height_m[0:5])
+print(np_weight_kg[0:3])
